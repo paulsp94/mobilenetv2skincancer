16a212aa5e527dff57279a59dc8936570d7ffdd1
Paul Spende <paul.spende@gmail.com>
2019-08-26 19:54:34 +0200

    refactoring and fixed code

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import { Image, createCanvas } from 'canvas';
-import * as tf from '@tensorflow/tfjs';
+import * as tf from '@tensorflow/tfjs-node';
 import Jimp from 'jimp';
 
 export default class SkinCancerPrediction {
   constructor() {
     this.model;
     this.classes = ['akiec', 'bcc', 'bkl', 'df', 'mel', 'nv', 'vasc'];
+    this.modelPath = `file:///${__dirname}/../model/model.json`;
   }
 
   async initialize() {
-    this.model = await tf.loadLayersModel(
-      'https://raw.githubusercontent.com/paulsp94/mobilenetv2skincancer/master/model/model.json'
-    );
+    this.model = await tf.loadLayersModel(this.modelPath);
   }
 
   static async create() {
@@ -20,47 +18,26 @@ export default class SkinCancerPrediction {
     return o;
   }
 
-  newLoadImg = async imgURI => {
-    const canvas = createCanvas(224, 224);
-    const ctx = canvas.getContext('2d');
-    try {
-      var img = new Image();
-      img.onload = () => ctx.drawImage(img, 0, 0);
-      img.onerror = err => {
-        throw err;
-      };
-      img.src = imgURI;
-      const image = tf.browser.fromPixels(canvas).reshape([1, 224, 224, 3]);
-      return image;
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
   loadImg = async imgURI => {
-    return Jimp.read(imgURI).then(() =>
-      tf.tidy(img => {
-        img.resize(224, 224);
-        const p = [];
-        img.scan(0, 0, img.bitmap.width, img.bitmap.height, function test(
-          x,
-          y,
-          idx
-        ) {
-          p.push(this.bitmap.data[idx + 0]);
-          p.push(this.bitmap.data[idx + 1]);
-          p.push(this.bitmap.data[idx + 2]);
-        });
+    return Jimp.read(imgURI).then(img => {
+      img.resize(224, 224);
+      const p = [];
+      img.scan(0, 0, img.bitmap.width, img.bitmap.height, function test(
+        x,
+        y,
+        idx
+      ) {
+        p.push(this.bitmap.data[idx + 0]);
+        p.push(this.bitmap.data[idx + 1]);
+        p.push(this.bitmap.data[idx + 2]);
+      });
 
-        return tf
-          .tensor3d(p, [img.bitmap.width, img.bitmap.height, 3])
-          .reshape([1, img.bitmap.width, img.bitmap.height, 3]);
-      })
-    );
+      return tf.tensor4d(p, [1, img.bitmap.width, img.bitmap.height, 3]);
+    });
   };
 
   classify = async imgURI => {
-    const img = await this.newLoadImg(imgURI);
+    const img = await this.loadImg(imgURI);
     const predictions = await this.model.predict(img);
     const prediction = predictions
       .reshape([7])
